45698b11dc4fd7004f5cff4cf6db2337728e15b7
Rohan Siva <rohansiva123@gmail.com>
2025-11-28 13:05:12 -0800

    add

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+
+
+$(document).ready(function () {
+    // Hover animation disabled - GIFs play continuously
+    // $('.publication-mouse-animate').mouseover(function () {
+    //     $(this).find('.animated').css('display', 'inline-block');
+    //     $(this).find('.static').css('display', 'none');
+    // });
+    // $('.publication-mouse-animate').mouseout(function () {
+    //     $(this).find('.animated').css('display', 'none');
+    //     $(this).find('.static').css('display', 'inline-block');
+    // });
+})
